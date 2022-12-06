25c06aecb277a9d5670d837b20d235d801048d42
mowalky <mowalky@outlook.com>
2022-12-05 21:53:27 -0600

    description promise

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return <div></div>;
+const fetchDescription = () =>
+  new Promise((resolve) =>
+    setTimeout(() => resolve("Product information ready for SEO"), 100)
+  );
+
+export default async function Home() {
+  const description = (await fetchDescription()) as string;
+
+  return (
+    <>
+      <header>Header</header>
+
+      <h2>Product Description</h2>
+      <p>{description}</p>
+
+      <h2>Comments</h2>
+
+      <footer>Footer</footer>
+    </>
+  );
 }
